2b44c2a66084f8141c4f6fb7ed42c9e7a102556c
bluur <bryantluu22@gmail.com>
2016-01-27 23:16:43 -0800

    add todo service

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {Component} from 'angular2/core';
+// this is a component
 import {TodoInput} from "./app/components/todo-input";
+
+// this is a service
+import {TodoService} from "./app/services/todo-service";
+
 
 @Component({
   selector: 'app',
@@ -9,5 +14,6 @@ import {TodoInput} from "./app/components/todo-input";
 })
 class App{}
 
-bootstrap(App)
+// we inject TodoService which tells angular any component inside app can use this service
+bootstrap(App, [TodoService])
   .catch(err => console.error(err));
--- src/app/components/todo-input.ts
+++ src/app/components/todo-input.ts
@@ -1,17 +1,23 @@
 import {Component} from 'angular2/core';
+import {TodoService} from "../services/todo-service";
 
 @Component({
   selector: 'todo-input',
   template: `<div>
   I'm a todo input
   <input type="text" #myInput>
-  <button (click)="onClick(myInput)">Click me</button>
+  <button (mouseover)="onClick($event, myInput.value)">Click me</button>
   </div>
   `
 })
 
 export class TodoInput{
-  onClick(value){
-    console.log(value);
+  // equal to this.todoService = todoService
+  constructor(public todoService:TodoService){
+    
+  }
+  onClick(event, value){
+    this.todoService.todos.push(value);
+    console.log(this.todoService.todos);
   }
 }
